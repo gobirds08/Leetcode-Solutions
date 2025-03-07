c548ee1cf398d6b96f66a06680ba11adb365179c
Brendan Kenney <brendankenney3@gmail.com>
2025-03-06 19:08:46 -0500

    somehow made worse

--- FindMissingAndRepeatedValues.cpp
+++ FindMissingAndRepeatedValues.cpp
@@ -1,32 +1,35 @@
 #include <vector>
 #include <map>
+#include <set>
 
 class Solution {
     public:
         std::vector<int> findMissingAndRepeatedValues(std::vector<std::vector<int>>& grid) {
             const std::size_t n = grid.size();
-            std::map<int, int> values;
-            
-            for (int i = 1; i <= n * n; i++) {
-                values.insert({i, 0});
-            }
-
-            for (auto arr : grid) {
-                for (auto num: arr) {
-                    values[num]++;
-                }
-            }
-
+            std::set<int> values;
             int repeated = 0;
-            int missing = 0;
-            for (auto pair : values) {
-                if (pair.second == 0) {
-                    missing = pair.first;
-                } else if (pair.second == 2) {
-                    repeated = pair.first;
+            int expec_accumulator = 0;
+            int got_accumulator = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (values.find(grid[i][j]) != values.end()) {
+                        repeated = grid[i][j];
+                    } else {
+                        values.insert(grid[i][j]);
+                        got_accumulator += grid[i][j];
+                    }
+                    expec_accumulator += (i * n) + j + 1;
                 }
             }
 
-            return {repeated, missing};
+            int missing = expec_accumulator - got_accumulator;
+        
+            return {repeated, missing};    
         }
 };
+
+int main() {
+    Solution solution;
+    std::vector<std::vector<int>> grid {{9,1,7},{8,9,2},{3,4,6}};
+    solution.findMissingAndRepeatedValues(grid);
+}
